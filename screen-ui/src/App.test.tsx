828860fd2f33318306e55979391a3d27174c2f52
Louis Holdsworth <louisholdsworth@gmail.com>
2022-06-19 19:24:44 +0100

    Added basic react app with tailwind

---
+++ screen-ui/src/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+
+// TODO write some tests... when there is stuff to test
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ screen-ui/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="bg-white min-h-screen dark:bg-slate-800">
+      <button onClick={() => setCount(count + 1)}>
+        Click me pls
+      </button>
+      <h1 className="text-3xl dark:text-white font-bold underline">Hello World! {count}</h1>
+    </div>
+  );
+}
+
+export default App;
+ 
