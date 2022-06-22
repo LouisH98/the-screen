8c4b1d4df4fcceef8e56288112521eb67413e3ff
Louis Holdsworth <louisholdsworth@gmail.com>
2022-06-22 22:54:31 +0100

    Formatting

--- screen-ui/src/App.tsx
+++ screen-ui/src/App.tsx
@@ -1,37 +1,33 @@
-import { useEffect, useState } from 'react';
-import { NextUIProvider, createTheme } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import './App.css';
+import { useEffect, useState } from "react";
+import { NextUIProvider, createTheme } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import "./App.css";
 
-import { getSlides, getStatus } from './utils/ScreenAPI';
-import { ScreenStatus } from './utils/interfaces';
-import { SetSlide } from './components/SetSlide';
+import { getSlides, getStatus } from "./utils/ScreenAPI";
+import { ScreenStatus } from "./utils/interfaces";
+import { SetSlide } from "./components/SetSlide";
 // 2. Call `createTheme` and pass your custom values
 const lightTheme = createTheme({
-  type: 'light'
-
-})
+  type: "light",
+});
 
 const darkTheme = createTheme({
-  type: 'dark',
+  type: "dark",
   theme: {
     fonts: {
       sans: "'Press Start 2P', cursive;",
-      cursive: "'Press Start 2P', cursive;"
-    }
-  }
-})
+      cursive: "'Press Start 2P', cursive;",
+    },
+  },
+});
 function App() {
-
   const [status, setStatus] = useState<ScreenStatus>();
   const [slides, setSlides] = useState<string[]>([]);
 
-
-  async function init(){
+  async function init() {
     setStatus(await getStatus());
     setSlides(await getSlides());
   }
-
 
   useEffect(() => {
     init();
@@ -39,24 +35,26 @@ function App() {
 
   return (
     <NextThemesProvider
-    defaultTheme="dark"
-    attribute="class"
-    value={{
-      light: lightTheme.className,
-      dark: darkTheme.className
-    }}
-  >
-  <NextUIProvider>
-    <div className="h-screen flex items-center flex-col">
-      <div id='app-wrapper' className='mt-5'>
-        <SetSlide getStatus={async () => setStatus(await getStatus())} allSlides={slides} currentSlide={status?.slide}/>
-      </div>
-    </div>
-  </NextUIProvider>
-</NextThemesProvider>
-
+      defaultTheme="dark"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <div className="h-screen flex items-center flex-col">
+          <div id="app-wrapper" className="mt-5">
+            <SetSlide
+              getStatus={async () => setStatus(await getStatus())}
+              allSlides={slides}
+              currentSlide={status?.slide}
+            />
+          </div>
+        </div>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
 
 export default App;
- 
