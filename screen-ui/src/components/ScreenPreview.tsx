f13434aad2f7cd255497027763680ba6493314f1
Louis Holdsworth <louisholdsworth@gmail.com>
2022-06-26 17:32:39 +0100

    Removed prints, cleaned up screen preview

--- screen-ui/src/components/ScreenPreview.tsx
+++ screen-ui/src/components/ScreenPreview.tsx
@@ -1,49 +1,80 @@
-import { useEffect, useState } from "react";
+import { Card } from "@nextui-org/react";
+import { useEffect, useRef, useState } from "react";
 import { getScreenData } from "../utils/ScreenAPI";
-// export function PixelRow(row){
-//   return row.map(pixelRow => {
-//     return pixelRow.forEach(pixel => {
-//       return <div style={{width: '2px', height: '2px', backgroundColor: 'rgb(pixel)'}}></div>
-//     })
-//   })
-// }
 
 export function ScreenPreview() {
   const [screenData, setScreenData] = useState([]);
+  const [isListening, setIsListening] = useState(true);
+  let stopEvents = useRef(() => {});
 
-  useEffect(() => {
-    getScreenData((event: MessageEvent<any>) => {
+  function startListening() {
+    const stopReceivingEvents = getScreenData((event: MessageEvent<any>) => {
       setScreenData(JSON.parse(event.data));
     });
+    setIsListening(true);
+    stopEvents.current = stopReceivingEvents;
+  }
+
+  function stopListening() {
+    stopEvents.current();
+    setIsListening(false);
+  }
+
+  useEffect(() => {
+    startListening();
+
+    return () => {
+      stopListening();
+    };
   }, []);
 
   return (
-    <div>
-      <table
-        className="m-0"
-        style={{ width: "200px", height: "200px" }}
-        id="screen-preview"
+    <div
+      style={{ display: "flex" }}
+      className="m-2 justify-center items-center"
+    >
+      <Card
+        onPress={() => (isListening ? stopListening() : startListening())}
+        variant="shadow"
+        isHoverable
+        isPressable
+        css={{ width: "226px" }}
       >
-        {screenData.map((items: number[]) => {
-          return (
-            <tr className="">
-              {items.map((pixels) => {
-                return (
-                  <td>
-                    <div
-                      style={{
-                        width: "12px",
-                        height: "12px",
-                        backgroundColor: `rgb(${pixels})`,
-                      }}
-                    />
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </table>
+        <table
+          className="m-0 p-5"
+          style={{
+            width: "200px",
+            height: "200px",
+            backgroundColor: "black",
+            filter: isListening ? "grayscale(0)" : "grayscale(1)",
+            transition: "filter 0.5s",
+          }}
+          id="screen-preview"
+        >
+          <tbody>
+            {screenData.map((items: number[], index) => {
+              return (
+                <tr key={index}>
+                  {items.map((pixels, iIndex) => {
+                    return (
+                      <td key={iIndex}>
+                        <div
+                          style={{
+                            width: "12px",
+                            height: "12px",
+                            borderRadius: "100%",
+                            backgroundColor: `rgb(${pixels})`,
+                          }}
+                        />
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
     </div>
   );
 }
