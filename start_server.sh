02d7c5ee620883291a36e430a1c1ea2370f33348
Louis Holdsworth <louisholdsworth@gmail.com>
2022-11-28 11:38:22 +0000

    Update run script to prevent logging to disk

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-nohup gunicorn -w 1 --worker-connections 1 -b 0.0.0.0 -k uvicorn.workers.UvicornWorker main:app &
+nohup gunicorn -w 1 --worker-connections 1 -b 0.0.0.0 -k uvicorn.workers.UvicornWorker main:app >/dev/null 2>&1 &
